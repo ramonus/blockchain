e09b3a3bc8b8ff3b06f4703ecf2552c44cb975b2
ramonus <ralmatob@gmail.com>
2017-12-18 18:10:44 +0100

    Configuring blockchain...

---
+++ dbm.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 18 10:25:18 2017
+
+@author: root
+"""
+import json,os, codecs
+from AES import *
+
+dbn = "db.json"
+keyn = "key.json"
+to_hex = lambda x: codecs.decode(x,"hex_codec").decode()
+to_barr = lambda x: codecs.encode(x,"hex_codec")
+def loadkey():
+  if not os.path.exists(keyn):
+    aes = AESC()
+    with open(keyn,"wb") as f:
+      f.write(aes.key)
+      key = aes.key
+  else:
+    with open(keyn,"rb") as f:
+      key = f.read()
+  return key
+def loadBC():
+  key = loadkey()
+  aes = AESC(key)
+  if not os.path.exists(dbn):
+    with open(dbn,"wb") as f:
+      f.write(aes.encrypt(json.dumps([],sort_keys=True)))
+  with open(dbn,"rb") as f:
+    return json.loads(aes.decrypt(f.read()))
+def saveBC(chain):
+  key = loadkey()
+  aes = AESC(key)
+  with open(dbn,"wb") as f:
+    f.write(aes.encrypt(json.dumps(chain,sort_keys=True)))
+  print("Chain with {} blocks saved".format(len(chain)))
+def deleteBC():
+  if os.path.exists(dbn):
+    os.remove(dbn)
+    print("BlockChain deleted")
--- manager.py
+++ manager.py
@@ -2,4 +2,39 @@
 
 # -*- coding: utf-8 -*-
 
-import hashlib
+import hashlib, json
+from dbm import *
+
+sha256 = lambda x: hashlib.sha256(json.dumps(x,sort_keys=True).encode()).hexdigest()
+class BManager:
+  def __init__(self,validator=None):
+    self.validator = validator
+    self.chain = loadBC()
+  def createBlock(self,tokens):
+    if len(self.chain)==0:
+      # generate a genesis block
+      print("Starting chain with a genesis block...")
+      gblock = {"block_n":0,
+               "parent_hash":None,
+               "token_n":len(tokens),
+               "tokens":tokens,
+               "hash":sha256(tokens)
+               }
+      print("Done!")
+      return gblock
+    else:
+      #non genesis block
+      print("Creating a new block...")
+      block = {"block_n":len(self.chain),
+               "parent_hash":sha256(self.chain[-1]),
+               "token_n":len(tokens),
+               "tokens":tokens,
+               "hash":sha256(tokens)
+               }
+      print("Done!")
+      return block
+  def addBlock(self,block):
+    if (self.validator != None and self.validator(self.chain,block)) or self.validator == None:
+      self.chain.append(block)
+      saveBC(self.chain)
+      print("Block added")
